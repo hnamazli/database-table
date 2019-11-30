6f8e58538b8a3d450ff257f61feb0c75a6fc4fc4
RaLLy08 <uytemataaero@mail.ru>
2019-11-30 22:50:08 +0400

    Rest Created

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,13 @@
+//import {sendGet, sendPut} from './REST';
+
+import * as rest from './REST'; //for tests
+
 import Model from "./Model";
 
 class Controller {
     
 }
 
-export default Model;
+window.rest = rest; //for tests
+
+export default Controller;
