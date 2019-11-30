a15bc43d81c1840419bb71a65dd6ac7ed9986ec1
eminmirzoev <m.emin123@live.com>
2019-11-30 13:31:53 +0400

    modul check please

--- src/Model.js
+++ src/Model.js
@@ -1,3 +1,19 @@
 class Model {
-    
+    constructor() {
+
+    }
 }
+
+let persons = [];
+console.log(Array.isArray(persons));
+let newPersons
+
+function addFirst() {
+    newPersons = document.getElementsByTagName("input")[0].value;
+    persons.unshift( newPersons );
+    document.getElementById("persons").innerHTML = persons;
+}
+function addLast() {
+    newPersons = document.getElementsByTagName("input")[0].value;
+    persons.push( newPersons );
+    document.getElementById("persons").innerHTML = persons;
