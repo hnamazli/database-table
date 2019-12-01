042227312525da10538b13bce09f3fcb4821f063
RaLLy08 <uytemataaero@mail.ru>
2019-12-01 15:28:20 +0400

    del unc

--- src/Model.js
+++ src/Model.js
@@ -2,16 +2,7 @@ class Model {
 
     constructor() {
         this._persons = [
-            {
-                firstName: 'Musa',
-                lastName: 'Xudiyev',
-                age: 25
-            },
-            {
-                firstName: 'Azad',
-                lastName: 'Kicikbekov',
-                age: 15
-            }
+
         ];
     }
 
